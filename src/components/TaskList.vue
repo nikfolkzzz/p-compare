a47436a9e8d245d772f3c7a05ead9b48b93f9d5d
Andru Nikolaev <aeanst514@yandex.ru>
2020-11-01 19:36:32 +0400

    create Task List

---
+++ src/components/TaskList.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="todo">
+    <ul class="todo__list" v-for="todo in todos" :key=" todo.id" >
+      <li class=" todo__item" > {{todo.title}} </li>
+
+    </ul>
+
+  </div>
+</template>
+
+
+<script>
+export default {
+  props:{
+    todos:Array
+  }
+}
+</script>
+
+
+<style scoped>
+.todo{
+  display: flex;
+  flex-direction: column;
+}
+
+
+.todo__list{
+
+}
+.todo__item{
+  display: flex;
+  justify-content: flex-start;
+}
+</style>
